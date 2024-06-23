9470ff3f8840408c20b23948947a45d89882ba77
helpingleaders <helping.leaders1@gmail.com>
2024-06-23 19:13:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,104 @@
+// Game variables, Tile Class, DOM references (Same as the previous response)
+
+window.addEventListener('message', (event) => {
+    if (event.data.type === 'init') {
+        $w.onReady(function () {
+            // UI Creation Functions 
+            function createGameBoardUI() {
+                // ... (same as before)
+            }
+            
+            function createTileUI(tile) {
+                let tileDiv = $w(`<div class="tile ${tile.type}">${tile.value}</div>`);
+                tile.element = tileDiv;
+                playerHandDiv.append(tileDiv);
+                tileDiv.on('click', () => {
+                    selectedTile = tile;
+                    $w('.tile').removeClass('selected');
+                    tileDiv.addClass('selected');
+                    if (tile.isWildcard) {
+                        // Show wildcard selection menu (implement this logic)
+                    }
+                });
+            }
+
+            // Update UI Functions (Implement these based on your design)
+            function updateGameBoardDisplay() {
+                // ... (Update game board UI based on gameBoard array)
+            }
+            
+            function updatePlayerHandDisplay() {
+                // ... (Update player hand UI based on playerHand array)
+            }
+            
+            function updateScoreDisplay() {
+                scoreDisplay.text = "Score: " + score;
+            }
+
+            // Generate Tiles
+            function generateTiles() {
+                let tiles = [];
+                for (let i = 0; i < 10; i++) {
+                    for (let j = 0; j < 2; j++) { // 2 of each number
+                        tiles.push(new Tile(i, 'number'));
+                    }
+                }
+                for (let op of operators) {
+                    for (let j = 0; j < 2; j++) { // 2 of each operator
+                        tiles.push(new Tile(op, 'operator'));
+                    }
+                }
+                let numWildcards = Math.floor(tiles.length * wildcardRatio);
+                for (let i = 0; i < numWildcards; i++) {
+                    tiles.push(new Tile('?', 'wildcard'));
+                }
+                return shuffle(tiles); 
+            }
+
+            // Shuffle function (Fisher-Yates algorithm)
+            function shuffle(array) {
+                for (let i = array.length - 1; i > 0; i--) {
+                    const j = Math.floor(Math.random() * (i + 1));
+                    [array[i], array[j]] = [array[j], array[i]];
+                }
+                return array;
+            }
+            
+            // Draw Tiles for Player's Hand
+            function drawTiles() {
+                while (playerHand.length < numTilesInHand) {
+                    if (generateTiles().length === 0) {
+                        endGame(); // No more tiles to draw, game over
+                        return;
+                    }
+                    playerHand.push(generateTiles().pop()); 
+                }
+
+                updatePlayerHandDisplay();
+            }
+
+
+            // Handle Tile Placement
+            function handleCellClick(row, col) {
+                if (selectedTile && !gameOver) {
+                    if (isValidPlacement(selectedTile, row, col)) {
+                        placeTile(selectedTile, row, col);
+                    } else {
+                        // Invalid move: Display error message briefly
+                        errorMessage.text = "Invalid placement!";
+                        setTimeout(() => errorMessage.text = "", 1500);
+                    }
+                }
+            }
+
+            // ... (placeTile, isValidPlacement, getEquation, calculateScore, getValidEquations, isPrime, areValidMovesLeft, endGame, startGame as before)
+        });
+    });
+
+    // Send message to Wix to indicate the iframe is loaded
+    window.parent.postMessage({ type: 'iframeLoaded' }, "*"); 
+</script>
+
+</body>
+</html>
 
